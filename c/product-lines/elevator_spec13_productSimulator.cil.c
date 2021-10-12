5da40518aa2bd9375d6bb4a7b641f1323b03f73c
Philipp Wendler <uni@philippwendler.de>
2021-10-12 14:17:08 +0200

    Convert license declaration of c/product-lines to REUSE format

--- c/product-lines/elevator_spec13_productSimulator.cil.c
+++ c/product-lines/elevator_spec13_productSimulator.cil.c
@@ -1,3 +1,11 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2011-2013 Alexander von Rhein, University of Passau
+// SPDX-FileCopyrightText: 2011-2021 The SV-Benchmarks Community
+//
+// SPDX-License-Identifier: Apache-2.0
+
 extern void abort(void);
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
 void reach_error() { __assert_fail("0", "elevator_spec13_productSimulator.cil.c", 3, "reach_error"); }
